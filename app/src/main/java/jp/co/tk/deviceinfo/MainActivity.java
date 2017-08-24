36fe0379c6966f81151227a2f48b91460119f1ed
上小路孝弘 <kamikouji@comworth.co.jp>
2017-08-24 12:16:33 +0900

    Fist Commit

---
+++ app/src/main/java/jp/co/tk/deviceinfo/MainActivity.java
@@ -0,0 +1,18 @@
+package jp.co.tk.deviceinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        main();
+    }
+
+    public void main() {
+
+    }
+}
